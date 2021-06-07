9026beae73e8260d8725c2627b00817ca04441cc
Lee Sangseok <JP21248@w010D71901183m.local>
2021-06-07 11:21:26 +0900

    Version up K8S Cluster

--- scripts/kubernetes-cluster.tf
+++ scripts/kubernetes-cluster.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   location            = var.location
   resource_group_name = azurerm_resource_group.flixtube.name
   dns_prefix          = var.app_name
-  kubernetes_version  = "1.18.14"
+  kubernetes_version  = "1.18.19"
 
   linux_profile {
     admin_username = var.admin_username
